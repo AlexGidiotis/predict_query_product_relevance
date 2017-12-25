467811b6e4a673e178c349a1f3df74b49ad1c5f0
Alex Gidiotis <gidiotisAlex@outlook.com.gr>
2017-12-25 21:21:33 +0200

    added a keras model

---
+++ python/data_prep_words.py
@@ -0,0 +1,127 @@
+from pyspark.sql import SparkSession
+from pyspark.ml.feature import Tokenizer, StopWordsRemover, HashingTF, IDF, VectorAssembler, CountVectorizer
+from pyspark.sql.functions import udf, col, size, lit, concat
+from pyspark.sql.types import *
+from pyspark.ml.regression import RandomForestRegressor, LinearRegression
+from pyspark.ml.evaluation import RegressionEvaluator
+
+from itertools import chain
+import re
+import json
+import time
+
+from keras.preprocessing.text import Tokenizer as Keras_Tokenizer
+
+
+keras_tokenizer = Keras_Tokenizer(filters='')
+
+def replaceDigits(text):
+	"""
+	Takes a string of text and preprocesses it. The preprocessing includes integer replacement with '#', remove LaTex
+	math and macros, tokenizing, stopword removal and encoding to ascii.
+
+	Argument:
+		text: A string.
+	Returns:
+		filtered_tokens: A list of the tokens.
+	"""
+	return re.sub('\d+','#',text)
+
+def removePunctuation(text):
+	"""
+	"""
+	return re.sub('[^a-zA-Z# ]','',text)
+
+
+def convert_to_int(wrd2id):
+	"""
+	Converts word tokens from string to int using the word2id dictionary. 
+	"""
+	def convert_to_int_(col):
+		keras_tokenizer.word_index = wrd2id
+		token_seq_int = keras_tokenizer.texts_to_sequences(col)
+		return [token for sublist in token_seq_int for token in sublist]
+	return udf(convert_to_int_,ArrayType(IntegerType(),False))
+
+
+data_path = '../input/train.csv'
+spark = SparkSession.builder.getOrCreate()
+
+min_word_TF = 10
+min_word_DF = 2
+vocabulary_size = 30000
+
+start_time = time.time()
+
+df = spark.read.csv(data_path,
+	header=True)
+
+df = df.withColumn('new_relevance',df['relevance'].cast(FloatType()))
+
+replaceDigitsUdf = udf(replaceDigits, StringType())
+df = df.withColumn('nodigits_title', replaceDigitsUdf('product_title'))
+df = df.withColumn('nodigits_sterm', replaceDigitsUdf('search_term'))
+
+removePunctuationUdf = udf(removePunctuation, StringType())
+df = df.withColumn('processed_title', removePunctuationUdf('nodigits_title'))
+df = df.withColumn('processed_sterm', removePunctuationUdf('nodigits_sterm'))
+
+title_tokenizer = Tokenizer(inputCol="processed_title",
+	outputCol="title_tokens")
+sterm_tokenizer = Tokenizer(inputCol="processed_sterm",
+	outputCol="sterm_tokens")
+
+df = title_tokenizer.transform(df)
+df = sterm_tokenizer.transform(df)
+
+title_remover = StopWordsRemover(inputCol="title_tokens",
+	outputCol="filtered_title_tokens")
+sterm_remover = StopWordsRemover(inputCol="sterm_tokens",
+	outputCol="filtered_sterm_tokens")
+df = title_remover.transform(df)
+df = sterm_remover.transform(df)
+
+def concat(type):
+    def concat_(*args):
+        return list(chain(*args))
+    return udf(concat_, ArrayType(type))
+
+concat_string_arrays = concat(StringType())
+
+df = df.withColumn('joined_tokens',concat_string_arrays(col('filtered_title_tokens'),col('filtered_sterm_tokens')))
+
+print('Creating the dictionary...')
+cv = CountVectorizer(inputCol='joined_tokens',
+	minTF=min_word_TF,
+	minDF=min_word_DF,
+	vocabSize=vocabulary_size).fit(df)
+
+df.select('filtered_title_tokens','filtered_sterm_tokens','relevance').show()
+
+vocab = cv.vocabulary
+wrd2id = dict((w,i+1) for i,w in enumerate(vocab))
+print('Found %s unique tokens' % len(wrd2id))
+df = df.withColumn('int_title_tokens', convert_to_int(wrd2id)('filtered_title_tokens'))
+df = df.withColumn('int_sterm_tokens', convert_to_int(wrd2id)('filtered_sterm_tokens'))
+
+df.select('filtered_title_tokens','filtered_sterm_tokens','int_title_tokens','int_sterm_tokens','relevance').show()
+
+print 'writting indexes...'
+with open('data/word_index.json', 'w') as fp:
+	json.dump(wrd2id, fp)
+
+train_df, test_df = df.randomSplit([0.8, 0.2], seed=45)
+train_size = train_df.count()
+test_size =  test_df.count()
+
+print train_size,test_size
+
+train_df.select('int_title_tokens','int_sterm_tokens').write.json(path="data/train_set", mode='overwrite')
+test_df.select('int_title_tokens','int_sterm_tokens').write.json(path="data/test_set", mode='overwrite')
+train_df.select('relevance').write.json(path="data/train_set_labels", mode='overwrite')
+test_df.select('relevance').write.json(path="data/test_set_labels", mode='overwrite')
+
+spark.stop()
+
+end_time = time.time()
+print "--- Run time: %s seconds ---" % (end_time - start_time)
--- python/ml_model.py
+++ python/ml_model.py
@@ -1,6 +1,224 @@
 from pyspark.sql import SparkSession
+import numpy as np
+import json
+
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Model
+from keras.layers import Dense, Dropout, Convolution1D
+from keras.layers import Embedding, Flatten, Input
+from keras.layers import GlobalAveragePooling1D, GlobalMaxPooling1D, MaxPooling1D
+from keras.layers import concatenate
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.models import model_from_json
+from keras.optimizers import Adam
+from keras.constraints import maxnorm
+from keras import regularizers
+
+
+embedding_dim = 200
+glove_path = '/home/alex/Documents/Python/glove.6B/glove.6B.200d.txt'
+STAMP = 'relevance_predictor'
+
+
+def load_wrd_embeddings(glove_path,
+	embedding_dim):
+
+	json_file = open('data/word_index.json')
+	json_string = json_file.read()
+	wrd2id = json.loads(json_string)
+	vocab_size = len(wrd2id)
+	print "Found %s words in the vocabulary." % vocab_size
+
+	embedding_idx = {}
+	glove_f = open(glove_path)
+	for line in glove_f:
+		values = line.split()
+		wrd = values[0]
+		coefs = np.asarray(values[1:],
+			dtype='float32')
+		embedding_idx[wrd] = coefs
+	glove_f.close()
+	print "Found %s word vectors." % len(embedding_idx)
+
+	embedding_mat = np.zeros((vocab_size+1,embedding_dim))
+	for wrd, i in wrd2id.items():
+		embedding_vec = embedding_idx.get(wrd)
+		# words without embeddings will be left with zeros.
+		if embedding_vec is not None:
+			embedding_mat[i] = embedding_vec
+
+	print embedding_mat.shape
+	return embedding_mat, vocab_size
+
+
+def build_net(title_len,
+	sterm_len,
+	embedding_dims,
+	lr=0.001):
+	
+	embedding_mat,vocab_size = load_wrd_embeddings(glove_path,embedding_dims)
+
+	# Title block
+	input_layer_1 = Input(shape=(title_len,),
+		dtype='int32')
+
+	title_embedding_layer = Embedding(input_dim=vocab_size+1,
+		output_dim=embedding_dims,
+		input_length=title_len,
+		weights=[embedding_mat],
+		trainable=False)(input_layer_1)
+
+	title_drop1 = Dropout(0.3)(title_embedding_layer)
+
+	title_conv1 = Convolution1D(32, (2),
+		activation='relu',
+		padding='valid',
+		kernel_initializer='lecun_uniform',
+		kernel_constraint=maxnorm(3),
+		kernel_regularizer=regularizers.l2(0.0))(title_drop1)
+	title_conv1 = MaxPooling1D(2)(title_conv1)
+
+	title_conv2 = Convolution1D(32, (4),
+		activation='relu',
+		padding='valid',
+		kernel_initializer='lecun_uniform',
+		kernel_constraint=maxnorm(3),
+		kernel_regularizer=regularizers.l2(0.0))(title_drop1)
+	title_conv2 = MaxPooling1D(2)(title_conv2)
+
+	title_conv3 = Convolution1D(32, (8),
+		activation='relu',
+		padding='valid',
+		kernel_initializer='lecun_uniform',
+		kernel_constraint=maxnorm(3),
+		kernel_regularizer=regularizers.l2(0.0))(title_drop1)
+	title_conv3 = MaxPooling1D(2)(title_conv3)
+	
+	title_concat = concatenate([title_conv1, title_conv2, title_conv3], axis=1)
+
+	title_flat = Flatten()(title_concat)
+
+
+	# Sterm block
+	input_layer_2 = Input(shape=(sterm_len,),
+		dtype='int32')
+
+	sterm_embedding_layer = Embedding(input_dim=vocab_size+1,
+		output_dim=embedding_dims,
+		input_length=sterm_len,
+		weights=[embedding_mat],
+		trainable=False)(input_layer_2)
+
+	sterm_drop1 = Dropout(0.3)(sterm_embedding_layer)
+
+	sterm_conv1 = Convolution1D(32, (2),
+		activation='relu',
+		padding='valid',
+		kernel_initializer='lecun_uniform',
+		kernel_constraint=maxnorm(3),
+		kernel_regularizer=regularizers.l2(0.0))(sterm_drop1)
+	sterm_conv1 = MaxPooling1D(2)(sterm_conv1)
+
+	sterm_conv2 = Convolution1D(32, (4),
+		activation='relu',
+		padding='valid',
+		kernel_initializer='lecun_uniform',
+		kernel_constraint=maxnorm(3),
+		kernel_regularizer=regularizers.l2(0.0))(sterm_drop1)
+	sterm_conv2 = MaxPooling1D(2)(sterm_conv2)
+
+	sterm_conv3 = Convolution1D(32, (8),
+		activation='relu',
+		padding='valid',
+		kernel_initializer='lecun_uniform',
+		kernel_constraint=maxnorm(3),
+		kernel_regularizer=regularizers.l2(0.0))(sterm_drop1)
+	sterm_conv3 = MaxPooling1D(2)(sterm_conv3)
+	
+	sterm_concat = concatenate([sterm_conv1, sterm_conv2, sterm_conv3], axis=1)
+
+	sterm_flat = Flatten()(sterm_concat)
+
+	# Combined block
+	concat = concatenate([title_flat,sterm_flat], axis=1)
+
+	dense1 = Dense(512,
+		activation='relu',
+		kernel_initializer='lecun_uniform')(concat)
+
+	drop2 = Dropout(0.5)(dense1)
+
+	predictions = Dense(1, activation='relu')(drop2)
+
+	model = Model(inputs=[input_layer_1, input_layer_2], outputs=predictions)
+
+	adam = Adam(lr=lr,
+		decay=1e-5)
+
+	model.compile(loss='mse',
+		optimizer=adam,
+		metrics=[])
+
+	model.summary()
+
+	return model
+
+
+max_title_len = 15
+max_sterm_len = 10
 
 
 spark = SparkSession.builder.getOrCreate()
-df = spark.read.json('input/train_set')
-df.select('features').show(truncate=False)
+
+train_df = spark.read.json('data/train_set')
+train_labs_df = spark.read.json('data/train_set_labels')
+
+X_titles_train = [item.int_title_tokens for item in train_df.select('int_title_tokens').collect()]
+X_sterms_train = [item.int_sterm_tokens for item in train_df.select('int_sterm_tokens').collect()]
+y_train = [float(item.relevance) for item in train_labs_df.select('relevance').collect()]
+
+print('Average train title sequence length: {}'.format(np.mean(list(map(len, X_titles_train)), dtype=int)))
+print('Average train sterm sequence length: {}'.format(np.mean(list(map(len, X_sterms_train)), dtype=int)))
+
+X_titles_train = pad_sequences(X_titles_train,
+	maxlen=max_title_len,
+	padding='post',
+	truncating='post',
+	dtype='float32')
+
+X_sterms_train = pad_sequences(X_sterms_train,
+	maxlen=max_sterm_len,
+	padding='post',
+	truncating='post',
+	dtype='float32')
+
+print X_titles_train.shape, X_sterms_train.shape
+
+model = build_net(max_title_len,max_sterm_len,embedding_dim)
+
+model_json = model.to_json()
+with open("model/" + STAMP + ".json", "w") as json_file:
+    json_file.write(model_json)
+
+early_stopping = EarlyStopping(monitor='val_loss',
+    patience=5)
+bst_model_path = "model/" + STAMP + '.h5'
+model_checkpoint = ModelCheckpoint(bst_model_path,
+    monitor='val_loss',
+    verbose=1,
+    save_best_only=True,
+    save_weights_only=True)
+
+hist = model.fit([X_titles_train,X_sterms_train],y_train,
+	validation_split=0.2,
+	epochs=10,
+	batch_size=64,
+	shuffle=True,
+    callbacks=[early_stopping,model_checkpoint],
+    verbose=1)
+
+model.load_weights('model/' + STAMP + '.h5')
+predictions = model.predict([X_titles_train,X_sterms_train])
+
+print predictions[:20]
+print y_train[:20]
