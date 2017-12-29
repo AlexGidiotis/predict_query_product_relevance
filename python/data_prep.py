c2b5c30554ae7e42cdedbf14d7415b97314395b5
Alex Gidiotis <gidiotisAlex@outlook.com.gr>
2017-12-29 15:27:19 +0200

    new features experiments

--- python/data_prep.py
+++ python/data_prep.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import Tokenizer, StopWordsRemover, HashingTF, IDF, VectorAssembler	
-from pyspark.sql.functions import udf, col, size, lit
+from pyspark.sql.functions import udf, col, size, lit, concat, concat_ws, collect_list
 from pyspark.sql.types import *
 from pyspark.ml.regression import RandomForestRegressor, LinearRegression
 from pyspark.ml.evaluation import RegressionEvaluator, MulticlassClassificationEvaluator
@@ -9,7 +9,7 @@ from pyspark.mllib.classification import SVMWithSGD, LabeledPoint
 from pyspark.sql.types import Row
 from pyspark.ml.linalg import DenseVector
 
-
+from itertools import chain
 import re
 
 
@@ -39,10 +39,28 @@ def createClassLabel(relevance):
 
 
 data_path = '../input/train.csv'
+description_path = '../input/product_descriptions.csv'
+attributes_path = '../input/attributes.csv'
+
 spark = SparkSession.builder.getOrCreate()
 
 df = spark.read.csv(data_path,
 	header=True)
+
+#descr_df = spark.read.csv(description_path,
+#	header=True)
+
+attr_df = spark.read.csv(attributes_path,
+	header=True)
+
+#df = df.join(descr_df, ['product_uid'])
+#attr_df = attr_df.groupBy('product_uid').agg(f.concat_ws(" ", f.collect_list(attr_df.name))).show()
+
+attr_df = attr_df.select('product_uid','name').groupBy('product_uid').agg(concat_ws(' ',collect_list('name')).alias('attribute_names'))
+
+df = df.join(attr_df, ['product_uid'])
+
+df.show()
 
 df = df.withColumn('new_relevance',df['relevance'].cast(FloatType()))
 
@@ -54,54 +72,73 @@ df = df.withColumn('label',createClassLabelUdf('new_relevance'))
 
 df = df.withColumn('nodigits_title', replaceDigitsUdf('product_title'))
 df = df.withColumn('nodigits_sterm', replaceDigitsUdf('search_term'))
+df = df.withColumn('nodigits_attr', replaceDigitsUdf('attribute_names'))
+#df = df.withColumn('nodigits_product_description', replaceDigitsUdf('product_description'))
 
 removePunctuationUdf = udf(removePunctuation, StringType())
 df = df.withColumn('processed_title', removePunctuationUdf('nodigits_title'))
 df = df.withColumn('processed_sterm', removePunctuationUdf('nodigits_sterm'))
+df = df.withColumn('processed_attr', removePunctuationUdf('nodigits_attr'))
+#df = df.withColumn('processed_product_description', removePunctuationUdf('nodigits_product_description'))
+
 
 title_tokenizer = Tokenizer(inputCol="processed_title",
 	outputCol="title_tokens")
 sterm_tokenizer = Tokenizer(inputCol="processed_sterm",
 	outputCol="sterm_tokens")
+attr_tokenizer = Tokenizer(inputCol="processed_attr",
+	outputCol="attr_tokens")
+#product_description_tokenizer = Tokenizer(inputCol="processed_product_description",
+#	outputCol="product_description_tokens")
 
 df = title_tokenizer.transform(df)
 df = sterm_tokenizer.transform(df)
+df = attr_tokenizer.transform(df)
+#df = product_description_tokenizer.transform(df)
 
 title_remover = StopWordsRemover(inputCol="title_tokens",
 	outputCol="filtered_title_tokens")
 sterm_remover = StopWordsRemover(inputCol="sterm_tokens",
 	outputCol="filtered_sterm_tokens")
+attr_remover = StopWordsRemover(inputCol="attr_tokens",
+	outputCol="filtered_attr_tokens")
+#product_description_remover = StopWordsRemover(inputCol="product_description_tokens",
+#	outputCol="filtered_product_description_tokens")
 df = title_remover.transform(df)
 df = sterm_remover.transform(df)
+df = attr_remover.transform(df)
+#df = product_description_remover.transform(df)
 
-title_hashingTF = HashingTF(inputCol="filtered_title_tokens",
-	outputCol="title_rawFeatures",
-	numFeatures=32768)
-sterm_hashingTF = HashingTF(inputCol="filtered_sterm_tokens",
-	outputCol="sterm_rawFeatures",
-	numFeatures=32768)
+def concat(type):
+    def concat_(*args):
+        return list(chain(*args))
+    return udf(concat_, ArrayType(type))
 
-df = title_hashingTF.transform(df)
-df = sterm_hashingTF.transform(df)
+concat_string_arrays = concat(StringType())
 
-title_idf = IDF(inputCol="title_rawFeatures",
-	outputCol="title_features")
-sterm_idf = IDF(inputCol="sterm_rawFeatures",
-	outputCol="sterm_features")
+df = df.withColumn('joined_tokens',concat_string_arrays(col('filtered_title_tokens'),col('filtered_sterm_tokens'),col('filtered_attr_tokens')))
 
-title_idfModel = title_idf.fit(df)
-sterm_idfModel = sterm_idf.fit(df)
+joined_hashingTF = HashingTF(inputCol="joined_tokens",
+	outputCol="joined_rawFeatures",
+	numFeatures=16384)
 
-df = title_idfModel.transform(df)
-df = sterm_idfModel.transform(df)
+df = joined_hashingTF.transform(df)
 
-df.select('label','title_features','sterm_features').show()
+joined_idf = IDF(inputCol="joined_rawFeatures",
+	outputCol="features")
+
+joined_idfModel = joined_idf.fit(df)
+
+df = joined_idfModel.transform(df)
+
+'''
 assembler = VectorAssembler(
-	inputCols=['title_features','sterm_features'],
+	inputCols=['title_features','sterm_features','product_description_features'],
 	outputCol='features')
 
 df = assembler.transform(df)
-df.select('title_features','sterm_features','features','label').show()
+'''
+df.select('features','label').show()
 
 train_df, test_df = df.randomSplit([0.8, 0.2], seed=45)
 train_size = train_df.count()
@@ -109,7 +146,7 @@ test_size =  test_df.count()
 
 print train_size,test_size
 
-
+'''
 #========================================== Classification =====================================================
 # Naive Bayess
 
@@ -145,9 +182,9 @@ print('Root Mean Squared Error (RMSE) on test data = %g' % rmse)
 '''
 #============================================= Regression =======================================================
 lr = LinearRegression(labelCol='new_relevance',
-	maxIter=100,
+	maxIter=15,
 	regParam=0.2,
-	elasticNetParam=0.1)
+	elasticNetParam=0.0)
 
 # Fit the model
 model = lr.fit(train_df)
@@ -172,5 +209,5 @@ predictions = model.transform(test_df)
 predictions.select('prediction','new_relevance').show()
 rmse = evaluator.evaluate(predictions)
 print('Root Mean Squared Error (RMSE) on test data = %g' % rmse)
-'''
+
 spark.stop()
